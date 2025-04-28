a99ea6fcc3ca0e37f631d7c698736ef95a995d3b
DavidKaracsonyi123 <210141@htlw10.at>
2025-04-28 13:32:28 +0200

    code update

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,35 @@
 
 #include <Arduino.h>
 
+#define ADC_CHANNEL 0;
+
+ADMUX |= (1<<REFS0);
+ADMUX &= ~(REFS1);
+
+ADMUX |= 0b0000;
+
+
+
 
 void setup() {
   Serial.begin(115200);
-}
+
+  ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+  
+  ADMUX = (ADMUX & 0b11110000) | ADC_CHANNEL;
+
+  ADMUX |= _BV(REFS0);
+  }
 
 void loop() {
-  int poti_value = analogRead (A0);
 
-  float voltage = (poti_value * 5) / 1023;
+  ADCSRA |= _BV(ADSC);
+  if((ADCSRA & _BV(ADSC)) != 0);
+  {
+    int poti_value = ADC;
+  }
+
+  float voltage = (poti_value * 5.0) / 1023;
 
   Serial.print("Potentiometer analog value = ");
   Serial.print("voltage,2");
