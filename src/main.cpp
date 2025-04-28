5aa8df3f62621bbe85ff01e2a120fa1bd4a503ef
DavidKaracsonyi123 <210141@htlw10.at>
2025-04-28 13:14:52 +0200

    code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
+/*
+  David Karacsonyi
+  28.04.2025
+  ADC exercise
+*/
+
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int poti_value = analogRead (A0);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  float voltage = (poti_value * 5) / 1023;
+
+  Serial.print("Potentiometer analog value = ");
+  Serial.print("voltage,2");
+  Serial.println(" V");
 }
