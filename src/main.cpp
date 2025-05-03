74a36703355e8d168a5c0c86c042affb83d63c9b
DavidKaracsonyi123 <210141@htlw10.at>
2025-05-03 20:21:21 +0200

    added interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,41 @@
 /*
   David Karacsonyi
-  28.04.2025
+  28.04.2025 - 03.05.2025
   ADC exercise
 */
 
 #include <Arduino.h>
 
-#define ADC_CHANNEL 0;
+#define ADC_CHANNEL 0
+
+volatile int poti_value = 0;
 
 void setup() {
   Serial.begin(115200);
 
-  // enable ADC and set prescaler to 128 -> 125kHz @ 16MHz (best conversion rast is between 50 - 200kHz)
-  ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  ADMUX = (1 << REFS0) | (ADC_CHANNEL & 00001111);
 
-  // set ADC channel of multiplexer
-  ADMUX = (ADMUX & 0b11110000) | ADC_CHANNEL;
-  
-  // set internal 5V reference
-  ADMUX |= (1 << REFS0);
+  // enable ADC and set prescaler to 128 -> 125kHz @ 16MHz (best conversion rast is between 50 - 200kHz)
+  ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+  sei(); //activate global Interrupts
+
+  ADCSRA |= (1 << ADSC); //start first Conversion
 }
 
 void loop() {
-  //int poti_value = analogRead(A0);
-
-  ADCSRA |= (1 << ADSC);                // start ADC conversion
-  while((ADCSRA & (1 << ADSC)) != 0);   // ADSC is cleared when conversion is completed
-  int poti_value = ADC;               // read ADC value
-  
   float voltage = (poti_value * 5.0) / 1023;
 
   Serial.print("Potentiometer analog value = ");
-  Serial.print(voltage,2);
+  Serial.print(voltage, 2);
   Serial.println(" V");
 
-  delay(500);                         // show only 500ms new value
+  delay(500); // show only 500ms new value
+}
+
+ISR(ADC_vect) {
+  poti_value = ADC; //read value
+  ADCSRA |= (1 << ADSC); //start next Conversion
 }
 
 
