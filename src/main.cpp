e8deecaa71d232dcce86df249a2337c362e3b242
DavidKaracsonyi123 <210141@htlw10.at>
2025-05-04 17:54:36 +0200

    number change

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ volatile int poti_value = 0;
 void setup() {
   Serial.begin(115200);
 
-  ADMUX = (1 << REFS0) | (ADC_CHANNEL & 00001111);
+  ADMUX = (1 << REFS0) | (ADC_CHANNEL & 0b00001111);
 
   // enable ADC and set prescaler to 128 -> 125kHz @ 16MHz (best conversion rast is between 50 - 200kHz)
   ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
