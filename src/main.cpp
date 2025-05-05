7afceef926ba9182ae365ad20834c03870601e89
DavidKaracsonyi123 <210141@htlw10.at>
2025-05-05 09:45:51 +0200

    errors corrected

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,14 @@ volatile int poti_value = 0;
 void setup() {
   Serial.begin(115200);
 
-  ADMUX = (1 << REFS0) | (ADC_CHANNEL & 0b00001111);
-
-  // enable ADC and set prescaler to 128 -> 125kHz @ 16MHz (best conversion rast is between 50 - 200kHz)
+  // enable ADC, enable Interrupt and set prescaler to 128 -> 125kHz @ 16MHz (best conversion rast is between 50 - 200kHz)
   ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+  // set ADC channel of multiplexer
+  ADMUX = (ADMUX & 0b11110000) | ADC_CHANNEL;
+   
+  // set internal 5V reference
+  ADMUX |= (1 << REFS0);
 
   sei(); //activate global Interrupts
 
